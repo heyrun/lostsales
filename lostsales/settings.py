7864e178d877b77618ec58b04f8c942238d62f06
Aaron Ebake <ebakeaaron@gmail.com>
2020-09-20 23:49:36 +0100

    new path test

--- lostsales/settings.py
+++ lostsales/settings.py
@@ -149,9 +149,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  commented out cos of AWS
 # DONE DUE TO MODIFICATIONS FOR AWS
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = 'static_files'
+# STATIC_ROOT = 'static'  # used for AWS
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 IMPORT_EXPORT_USE_TRANSACTIONS = True
 
