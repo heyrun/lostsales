3dd83f057647d3cd7968e84416d6cbe433e317d4
Aaron Ebake <ebakeaaron@gmail.com>
2020-09-20 23:50:37 +0100

    new path test

--- lostsales/settings.py
+++ lostsales/settings.py
@@ -151,7 +151,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  commented out cos of AWS
 # DONE DUE TO MODIFICATIONS FOR AWS
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATIC_ROOT = 'static_files'
 # STATIC_ROOT = 'static'  # used for AWS
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
