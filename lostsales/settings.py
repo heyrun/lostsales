10c7732dc4f8a6a6dd48002f3d44081f7d280ffa
Aaron Ebake <ebakeaaron@gmail.com>
2020-09-21 00:13:51 +0100

    aa

--- lostsales/settings.py
+++ lostsales/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 #MEDIA_URL = '/images/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  commented out cos of AWS
 # DONE DUE TO MODIFICATIONS FOR AWS
