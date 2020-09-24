d09791180be9c84e557e49cb39201df01f236a15
Aaron Ebake <ebakeaaron@gmail.com>
2020-09-24 15:42:07 +0100

    Updated gitignore

--- lostsales/settings.py
+++ lostsales/settings.py
@@ -152,7 +152,7 @@ AWS_STORAGE_BUCKET_NAME = 'lostsales'
 AWS_S3_FILE_OVERWRITE = True
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 # # Static files (CSS, JavaScript, Images)
